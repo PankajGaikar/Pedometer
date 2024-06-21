c2aafa487633c8ecca51bcf2bc6358465a2e1292
Pankaj Gaikar <pankaj@orionlabs.io>
2024-06-21 15:07:31 +0530

    Created project.

---
+++ Pedometer/PedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedometerApp.swift
+//  Pedometer
+//
+//  Created by Pankaj Gaikar on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
